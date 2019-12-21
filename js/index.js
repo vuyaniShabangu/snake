f0ef9a868b3cb695257c08f8301db013f03b2417
Vuyani Shabangu <vuyani@jemstep.com>
2019-12-21 08:17:14 +0200

    displaying grid and snake

--- js/index.js
+++ js/index.js
@@ -1,9 +1,40 @@
 import {Snake} from './snake.js';
 
-setInterval(function(){ render(); }, 500);
-
+setInterval(function(){ render(); }, 1000);
 const board = document.getElementById('board');
 
-const render = () => {
-    board.innerHTML =  new Date();
+const changeToBlock =(x,y) => {
+    document.getElementById(x+","+y).className = "block";
 }
+
+const changeToSnakeBlock =(x,y) => {
+    document.getElementById(x+","+y).className = "snake-block";
+}
+
+const changeToSnakeHead =(x,y) => {
+    document.getElementById(x+","+y).className = "snake-head";
+}
+
+const changeToTarget =(x,y) => {
+    document.getElementById(x+","+y).className = "target";
+}
+
+
+
+let snake = new Snake();
+
+const render = () => {
+
+    changeToSnakeHead(50,50)
+    changeToSnakeBlock(49,50)
+    changeToSnakeBlock(48,50)
+    changeToSnakeBlock(47,50)
+    changeToSnakeBlock(46,50)
+    changeToSnakeBlock(45,50)
+
+    changeToTarget(68,68)
+
+   
+}
+
+render();
--- js/snake.js
+++ js/snake.js
@@ -7,10 +7,59 @@ export class Snake {
         for(var i=0; i < this.board.length; i++)
             this.board[i] = new Array(100);
 
-        this.path = [Block(50,50), Block(49,50), Block(48,50)]
+        this.board.map( column => { new Array(100) } )
+        
+        /*for(var x=0; x < this.board.length; x++)
+        {
+            for(var y=0; y < this.board.length; y++)
+            {
+                this.board[x][y] = 'x';
+            }
+        }*/
+
+
+        this.path = [new Block(50,50), new Block(49,50), new Block(48,50)];
+
+        this.direction = "right";
+
+        this.plotPath()
     }
 
+    plotPath(){
+        this.path.forEach( block => this.board[block.x][block.y] = block )
+    }
 
-    
+    displaySnake(){
+        return this.path;
+    }
+
+    changeDirection(dir){
+        this.direction = dir;
+    }
+
+    iterate(){
+        if(this.direction === "left")
+            this.moveLeft()
+        if(this.direction === "right")
+            this.moveRight()
+        if(this.direction === "up")
+            this.moveUp()
+        if(this.direction === "down")
+            this.moveDown()
+    }
+
+    moveUp(){
+        this.path.map( block => block.y++ )
+    }
+    moveDown(){
+        this.path.map( block => block.y-- )
+    }
+    moveLeft(){
+        this.path.map( block => block.x-- )
+    }
+    moveRigt(){
+        this.path.map( block => block.x++ )
+    }
+
     
 }
