6a202273d78baa2ba1b865353285038c7d8ce770
Vuyani Shabangu <vuyani.shabangu@gmail.com>
2021-09-04 16:58:09 +0200

    touch ups

--- js/snake.js
+++ js/snake.js
@@ -1,228 +1,217 @@
-import {Block} from './block.js';
-import {Pivot} from './pivot.js';
-import {Target} from './target.js';
+import { Block } from './block.js';
+import { Pivot } from './pivot.js';
+import { Target } from './target.js';
 
 
 export class Snake {
 
-    constructor(){
+    constructor() {
         this.board = new Array(30);
-        for(var i=0; i < this.board.length; i++)
+        for (var i = 0; i < this.board.length; i++)
             this.board[i] = new Array(30);
-        this.board.map( column => { new Array(30) } )
+        this.board.map(column => { new Array(30) })
         this.direction = "right";
-        this.path = [new Block(15,15,this.direction,"head"), new Block(14,15,this.direction), new Block(13,15,this.direction), new Block(12,15,this.direction), new Block(11,15,this.direction), new Block(10,15,this.direction, "tail")];
+        this.path = [new Block(15, 15, this.direction, "head"), new Block(14, 15, this.direction), new Block(13, 15, this.direction), new Block(12, 15, this.direction), new Block(11, 15, this.direction), new Block(10, 15, this.direction, "tail")];
         this.plotPath()
         this.pivots = new Array();
-        this.target = new Target(0,0,"");
+        this.target = new Target(0, 0, "");
         this.interval = 100;
         this.score = 0;
     }
 
-    plotPath(){
-        this.path.forEach( block => this.board[block.x][block.y] = block )
+    plotPath() {
+        this.path.forEach(block => this.board[block.x][block.y] = block)
     }
 
-    displayTarget(){
+    displayTarget() {
         return this.target;
     }
 
-    displaySnake(){
+    displaySnake() {
         return this.path;
     }
 
-    getInterval(){
+    getInterval() {
         return this.interval;
     }
 
-    changeDirection(dir){
+    changeDirection(dir) {
         this.direction = dir;
     }
 
-    reduceInterval(){
-        if(this.interval-5 >= 20)
+    reduceInterval() {
+        if (this.interval - 5 >= 20)
             this.interval -= 2;
     }
 
-    growSnake(){
+    growSnake() {
         let length = this.path.length;
-        let tail = {...this.path[length - 1]};
+        let tail = { ...this.path[length - 1] };
         this.path[length - 1].type = "snakeBody";
 
-        if(tail.direction === "up")
+        if (tail.direction === "up")
             tail.y++
-        if(tail.direction === "down")
+        if (tail.direction === "down")
             tail.y--
-        if(tail.direction === "right")
+        if (tail.direction === "right")
             tail.x--
-        if(tail.direction === "left")
+        if (tail.direction === "left")
             tail.x++
         this.path.push(tail);
     }
 
-    isTargetHit(head, target){
+    isTargetHit(head, target) {
         return (head.x === target.x && head.y === target.y);
     }
 
-    isValidDirectionChange(headDirection, newDirection){
+    isValidDirectionChange(headDirection, newDirection) {
         let valid = true;
-        if(headDirection === "up" && newDirection === "down")
+        if (headDirection === "up" && newDirection === "down")
             valid = false;
-        if(headDirection === "down" && newDirection === "up")
+        if (headDirection === "down" && newDirection === "up")
             valid = false;
-        if(headDirection === "left" && newDirection === "right")
+        if (headDirection === "left" && newDirection === "right")
             valid = false;
-        if(headDirection === "right" && newDirection === "left")
+        if (headDirection === "right" && newDirection === "left")
             valid = false;
-        
-        if(headDirection === "up" && newDirection === "down")
+
+        if (headDirection === "up" && newDirection === "down")
             valid = false;
-        if(headDirection === "down" && newDirection === "up")
+        if (headDirection === "down" && newDirection === "up")
             valid = false;
-        if(headDirection === "left" && newDirection === "right")
+        if (headDirection === "left" && newDirection === "right")
             valid = false;
-        if(headDirection === "right" && newDirection === "left")
+        if (headDirection === "right" && newDirection === "left")
             valid = false;
         return valid
     }
 
-    getScore(){
+    getScore() {
         return this.score;
     }
 
-    increaseScore(){
+    increaseScore() {
         this.score += 1;
     }
 
-    iterate(direction){
-        
-        if(this.isTargetHit(this.path[0], this.target)){
+    iterate(direction) {
+
+        if (this.isTargetHit(this.path[0], this.target)) {
             this.target.plotTarget();
             this.growSnake();
             this.reduceInterval()
             this.increaseScore();
         }
 
-        if(direction)
-        {
+        if (direction) {
             this.direction = direction;
             this.pivots.push(new Pivot(this.path[0].x, this.path[0].y, direction));
         }
-        this.path.map(block => {this.move(block)});
-        
+        this.path.map(block => { this.move(block) });
+
         this.isEndGame()
     }
 
-    isEndGame(){
+    isEndGame() {
         let endGame = false;
         let head = this.path[0];
         this.path.map(bodyPart => {
-            if(bodyPart.type !== "head" && bodyPart.x === head.x && bodyPart.y === head.y)
+            if (bodyPart.type !== "head" && bodyPart.x === head.x && bodyPart.y === head.y){
                 alert("GAME OVER!")
+                location.reload()
+            }
         });
     }
 
-    move(block){
-        if(block.type === "head" )
-        {   
-            if(this.isValidDirectionChange(block.direction, this.direction))
-            {
+    move(block) {
+        if (block.type === "head") {
+            if (this.isValidDirectionChange(block.direction, this.direction)) {
                 block.direction = this.direction
             }
             block = this.moveToCurrentDirection(block)
         }
-        else{
-            
+        else {
+
             let removeFirstPivot = false;
-            if(this.pivots.length === 0)
-            {
-                    block = this.moveToCurrentDirection(block)
+            if (this.pivots.length === 0) {
+                block = this.moveToCurrentDirection(block)
             }
-            else{
-                
+            else {
+
                 this.pivots.map(pivot => {
-                    if(pivot.x === block.x && pivot.y === block.y)
-                    {
-                        if(this.isValidDirectionChange(block.direction, pivot.direction))
+                    if (pivot.x === block.x && pivot.y === block.y) {
+                        if (this.isValidDirectionChange(block.direction, pivot.direction))
                             block.direction = pivot.direction;
-                        if(block.type === "tail")
+                        if (block.type === "tail")
                             removeFirstPivot = true;
-                    }                
-            });
+                    }
+                });
                 block = this.moveToCurrentDirection(block);
-                if(removeFirstPivot)this.pivots.shift() 
+                if (removeFirstPivot) this.pivots.shift()
             }
         }
         return block;
     }
 
-    moveSnake(){
+    moveSnake() {
         this.growSnake();
-        
+
     }
 
 
-    moveToCurrentDirection(block){
-        if(block.direction === "left" )
-            {
-                block = this.moveLeft(block)
-            }
-            if(block.direction === "right")
-            {
-                block = this.moveRight(block)
-            }
-            if(block.direction === "up")
-            {
-                block = this.moveUp(block)
-            }
-            if(block.direction === "down")
-            {
-                block = this.moveDown(block)
-            }
+    moveToCurrentDirection(block) {
+        if (block.direction === "left") {
+            block = this.moveLeft(block)
+        }
+        if (block.direction === "right") {
+            block = this.moveRight(block)
+        }
+        if (block.direction === "up") {
+            block = this.moveUp(block)
+        }
+        if (block.direction === "down") {
+            block = this.moveDown(block)
+        }
 
-            return block;
-    }
-
-    moveToPivotDirection(block, pivotDirection){
-            if(pivotDirection === "left")
-            {
-                block = this.moveLeft(block)
-            }
-            if(pivotDirection === "right")
-            {
-                block = this.moveRight(block)
-            }
-            if(pivotDirection === "up")
-            {
-                block = this.moveUp(block)
-            }
-            if(pivotDirection === "down")
-            {
-                block = this.moveDown(block)
-            }
-            block.direction = pivotDirection;
-            return block;
-    }
-
-    moveUp(block){
-        block.y = Math.abs((block.y-1 + 30)%30)
         return block;
     }
 
-    moveDown(block){
-        block.y = Math.abs((block.y+1)%30)
+    moveToPivotDirection(block, pivotDirection) {
+        if (pivotDirection === "left") {
+            block = this.moveLeft(block)
+        }
+        if (pivotDirection === "right") {
+            block = this.moveRight(block)
+        }
+        if (pivotDirection === "up") {
+            block = this.moveUp(block)
+        }
+        if (pivotDirection === "down") {
+            block = this.moveDown(block)
+        }
+        block.direction = pivotDirection;
         return block;
     }
 
-    moveLeft(block){
-        block.x = Math.abs((block.x-1 + 30)%30)
+    moveUp(block) {
+        block.y = Math.abs((block.y - 1 + 30) % 30)
         return block;
     }
 
-    moveRight(block){
-        block.x = Math.abs((block.x+1)%30)
+    moveDown(block) {
+        block.y = Math.abs((block.y + 1) % 30)
         return block;
     }
 
-    
+    moveLeft(block) {
+        block.x = Math.abs((block.x - 1 + 30) % 30)
+        return block;
+    }
+
+    moveRight(block) {
+        block.x = Math.abs((block.x + 1) % 30)
+        return block;
+    }
+
+
 }
